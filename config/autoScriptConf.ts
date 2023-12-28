5a6e7838482a29a7171014b1b1a90a4767e39861
anyway <lianghongliang@163.com>
2023-12-28 15:52:54 +0800

    feat: 抽离自动任务配置文件

---
+++ config/autoScriptConf.ts
@@ -0,0 +1,22 @@
+// 掘金
+const juejin = {
+  url: 'https://api.juejin.cn/growth_api/v1/check_in?aid=2608&uuid=7170885490388043297&spider=0&msToken=IIknwbAVAiSVpKHIwKVtsx1Kd8vx7M6BRaCkrq7Ymk2WYGdNy2K6TzHXdjsKbiihCmElL9dv7romSX-G18nljUIzYG1YLkDnij42PTuiE8in8X1gYDSkV1nlSt8dUZ95&a_bogus=my0mXchtMsm1cfVwJwDz9bwm8qD0YWRVgZEzBetwIzLD',
+  sessionid: 'aeb81595b2bde6369235bdb4ec7360e2',
+};
+
+// 钻芒
+const zm = {
+  cookie:
+    'wordpress_sec_9f31b74abcbd1c3b130564da32286f2f=anywayuan%7C1704505080%7C0jfvSffaabxpvVGoeNmXtQXzRS3ZuaqwIVC7IuZsWYS%7Cbfa1f489d5a085830971084598e0bbae5b2ac725923318f6023a0da70a1d7c85; __51vcke__JIXrSMd2TfFevxLS=86ef1a72-c064-57d8-a899-26fff5a1a5c7; __51vuft__JIXrSMd2TfFevxLS=1698986406367; __51huid__K2QVQraVo2bvLB8o=f7e99f01-e464-5b5c-b378-657d8f0d9fff; wordpress_logged_in_9f31b74abcbd1c3b130564da32286f2f=anywayuan%7C1704505080%7C0jfvSffaabxpvVGoeNmXtQXzRS3ZuaqwIVC7IuZsWYS%7C5369827220030b9c94ca7552e5c384e70ab80e1de823e7023c98cf73cc34b791; PHPSESSID=tru2frj1fn5fdgv9u9401dh4ai; Hm_lvt_0acbdfffd7e74560a42f779e64063225=1702532953,1703229029,1703295411,1703727907; __51uvsct__JIXrSMd2TfFevxLS=20; __vtins__JIXrSMd2TfFevxLS=%7B%22sid%22%3A%20%22ac5972df-cde6-5e00-a55f-3bbd37f2e625%22%2C%20%22vd%22%3A%202%2C%20%22stt%22%3A%205096%2C%20%22dr%22%3A%205096%2C%20%22expires%22%3A%201703750763042%2C%20%22ct%22%3A%201703748963042%7D; Hm_lpvt_0acbdfffd7e74560a42f779e64063225=1703748963',
+  url: 'https://www.zuanmang.net/wp-admin/admin-ajax.php',
+  action: 'user_checkin',
+};
+
+// 必应
+const bing = {
+  base: 'https://www.bing.com',
+  reqUrl:
+    'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=9&pid=hp&FORM=BEHPTB&uhd=1&uhdwidth=3840&uhdheight=2160&setmkt=%s&setlang=en',
+};
+
+export { juejin, zm, bing };
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -3,6 +3,7 @@ import { Cron } from '@nestjs/schedule';
 import { HttpService } from '@nestjs/axios';
 import { firstValueFrom } from 'rxjs';
 import * as fs from 'fs';
+import { juejin, zm, bing } from '../../config/autoScriptConf';
 
 @Injectable()
 export class ScheduleService {
@@ -19,14 +20,11 @@ export class ScheduleService {
    * @description: 掘金自动签到
    */
   AutoSignToJJ() {
-    const sessionid: string = 'aeb81595b2bde6369235bdb4ec7360e2'; // SessionID
-    const url: string =
-      'https://api.juejin.cn/growth_api/v1/check_in?aid=2608&uuid=7170885490388043297&spider=0&msToken=IIknwbAVAiSVpKHIwKVtsx1Kd8vx7M6BRaCkrq7Ymk2WYGdNy2K6TzHXdjsKbiihCmElL9dv7romSX-G18nljUIzYG1YLkDnij42PTuiE8in8X1gYDSkV1nlSt8dUZ95&a_bogus=my0mXchtMsm1cfVwJwDz9bwm8qD0YWRVgZEzBetwIzLD';
     const options = {
-      url,
+      url: juejin.url,
       method: 'post',
       headers: {
-        cookie: 'sessionid=' + sessionid,
+        cookie: 'sessionid=' + juejin.sessionid,
       },
     };
     firstValueFrom(this.httpService.request(options)).then((res) => {
@@ -38,19 +36,14 @@ export class ScheduleService {
    * @description: 钻芒自动签到
    */
   AutoSignToZM() {
-    const cookie: string =
-      'wordpress_sec_9f31b74abcbd1c3b130564da32286f2f=anywayuan%7C1703742560%7CHRVYfAYo7pjfeJNE2U4o8L577Ukl4HFPGutKra7SScp%7C0dd4b66efc43b7b14e43b0f81b8a73cc11bbaf3187033e1b0df8881f1e714f9b; __51vcke__JIXrSMd2TfFevxLS=86ef1a72-c064-57d8-a899-26fff5a1a5c7; __51vuft__JIXrSMd2TfFevxLS=1698986406367; __51huid__K2QVQraVo2bvLB8o=f7e99f01-e464-5b5c-b378-657d8f0d9fff; __51uvsct__JIXrSMd2TfFevxLS=13; PHPSESSID=ohodv75e27c2sk2d3m3bao6i91; Hm_lvt_0acbdfffd7e74560a42f779e64063225=1700537710,1701398878,1702258735,1702532953; wordpress_logged_in_9f31b74abcbd1c3b130564da32286f2f=anywayuan%7C1703742560%7CHRVYfAYo7pjfeJNE2U4o8L577Ukl4HFPGutKra7SScp%7C9f4222ae4e58816b0602b5cc77a8161ed2c6f7c804957a66ae01ab9dea5fa377; Hm_lpvt_0acbdfffd7e74560a42f779e64063225=1702533091'; // Cookie
-    const url: string = 'https://www.zuanmang.net/wp-admin/admin-ajax.php'; // 签到接口
-    const action: string = 'user_checkin'; // 行为 user_checkin代表签到 钻芒接口固定值
-
     const formData = new FormData();
-    formData.append('action', action);
+    formData.append('action', zm.action);
 
     const options = {
-      url: url,
+      url: zm.url,
       method: 'post',
       headers: {
-        cookie: cookie,
+        cookie: zm.cookie,
       },
       data: formData,
     };
@@ -71,18 +64,14 @@ export class ScheduleService {
       enddate: string;
     };
 
-    const base: string = 'https://www.bing.com';
-    const reqUrl: string =
-      base +
-      '/HPImageArchive.aspx?format=js&idx=0&n=9&pid=hp&FORM=BEHPTB&uhd=1&uhdwidth=3840&uhdheight=2160&setmkt=%s&setlang=en';
     const options = {
-      url: reqUrl,
+      url: bing.reqUrl,
       method: 'get',
     };
     let todayWallpaper = {} as TodayWallpaper;
     const res = await firstValueFrom(this.httpService.request(options));
     todayWallpaper = res.data.images[0] as TodayWallpaper;
-    const imageFullUrl: string = base + todayWallpaper.url; // 图片资源路径
+    const imageFullUrl: string = bing.base + todayWallpaper.url; // 图片资源路径
     // 下载资源
     const response = await firstValueFrom(
       this.httpService.request({
