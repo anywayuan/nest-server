c2bc9388590f7788230d6ea37c17000fd3729b72
anyway <lianghongliang@163.com>
2024-03-04 15:47:35 +0800

    feat: 新增根据分类获取对应图片资源

--- src/modules/light-and-you/light-and-you.controller.ts
+++ src/modules/light-and-you/light-and-you.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { LightAndYouService } from './light-and-you.service';
 import { Public } from 'src/global/decorator/public.decorator';
 import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { GetAllAlbumResDto } from './dto/get-album.dto';
+import { GetPhotosResDto } from './dto/get-photos.dto';
+import { GetPhotosReqDto } from './dto/get-photos.dto';
 
 @ApiTags('Sky And You')
 @Controller('wxmp')
@@ -15,5 +17,13 @@ export class LightAndYouController {
   @ApiResponse({ status: 200, description: 'success', type: GetAllAlbumResDto })
   async getAlbumList() {
     return await this.lightAndYouService.getAlbumList();
+  }
+
+  @Post('photos')
+  @Public()
+  @ApiOperation({ summary: '获取分类下图片资源' })
+  @ApiResponse({ status: 200, description: 'success', type: GetPhotosResDto })
+  async getPhotosByAlbum(@Body() body: GetPhotosReqDto) {
+    return await this.lightAndYouService.getPhotosByAlbum(body);
   }
 }
--- src/modules/light-and-you/light-and-you.module.ts
+++ src/modules/light-and-you/light-and-you.module.ts
@@ -3,9 +3,10 @@ import { LightAndYouService } from './light-and-you.service';
 import { LightAndYouController } from './light-and-you.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AlbumsEntity } from './entities/album.entity';
+import { PhotoEntity } from './entities/photo.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AlbumsEntity])],
+  imports: [TypeOrmModule.forFeature([AlbumsEntity, PhotoEntity])],
   providers: [LightAndYouService],
   controllers: [LightAndYouController],
 })
